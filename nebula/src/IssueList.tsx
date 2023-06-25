a973e5bada66c378f9d6b04de24601b59beb8899
kura <>
2023-06-25 12:32:44 +0900

    chunk

--- nebula/src/IssueList.tsx
+++ nebula/src/IssueList.tsx
@@ -7,16 +7,6 @@ interface GithubIssue {
     html_url: string;
 }
 
-async function fetchIssues(): Promise<GithubIssue[]> {
-    const response = await fetch('https://api.github.com/repos/kurari50/Nebula/issues');
-    const data = await response.json();
-    if (response.ok) {
-        return data;
-    } else {
-        throw new Error(data.message);
-    }
-}
-
 const IssueList: React.FC = () => {
     const [issues, setIssues] = useState<GithubIssue[]>([]);
     const [error, setError] = useState<string | null>(null);
@@ -37,16 +27,30 @@ const IssueList: React.FC = () => {
 
     return (
         <div>
-            {issues.map(issue => (
-                <div key={issue.id}>
-                    <h2>
-                        <a href={issue.html_url} target="_blank" rel="noopener noreferrer">{issue.title}</a>
-                    </h2>
-                    <p>{issue.body}</p>
-                </div>
-            ))}
+            {issues.map(renderIssue)}
         </div>
     );
+}
+
+function renderIssue(issue: GithubIssue) {
+    return (
+        <div key={issue.id}>
+            <h2>
+                <a href={issue.html_url} target="_blank" rel="noopener noreferrer">{issue.title}</a>
+            </h2>
+            <p>{issue.body}</p>
+        </div>
+    )
+}
+
+async function fetchIssues(): Promise<GithubIssue[]> {
+    const response = await fetch('https://api.github.com/repos/kurari50/Nebula/issues');
+    const data = await response.json();
+    if (response.ok) {
+        return data;
+    } else {
+        throw new Error(data.message);
+    }
 }
 
 export default IssueList;
