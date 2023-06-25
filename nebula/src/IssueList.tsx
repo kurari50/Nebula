98c5fb47d0fd30cc4725d9d1ef929081220764ef
kura <>
2023-06-25 19:07:20 +0900

    Issueのbodyを改行して表示するように修正

--- nebula/src/IssueList.tsx
+++ nebula/src/IssueList.tsx
@@ -37,6 +37,21 @@ const IssueList: React.FC = () => {
     return <div>{issues.map(renderIssue)}</div>;
 };
 
+// 改行コードで文字列を分割する
+function splitLines(str: string): string[] {
+    return str.split("\n");
+}
+
+// 改行コードを削除する
+function removeCRLN(str: string): string {
+    return str.replace(/\r?\n/g, "");
+}
+
+// 文字列をpタグで囲む
+function stringToP(str: string): JSX.Element {
+    return <p>{`${str}`}</p>;
+}
+
 // stateに応じてコンポーネントの描画を行う
 function renderIssue(issue: GithubIssue) {
     console.log("Rendering issue " + issue.id);
@@ -51,7 +66,9 @@ function renderIssue(issue: GithubIssue) {
                     {issue.title}
                 </a>
             </h2>
-            <p>{issue.body}</p>
+            {splitLines(issue.body ?? "")
+                .map(removeCRLN)
+                .map(stringToP)}
         </div>
     );
 }
