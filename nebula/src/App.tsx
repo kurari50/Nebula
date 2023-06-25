984dd69247ae2b65fd2cfeabc5886b50e0920500
kura <>
2023-06-25 11:04:43 +0900

    react-router-domを使って書き換え

--- nebula/src/App.tsx
+++ nebula/src/App.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Routes, Route} from "react-router-dom";
+import Navbar from './Navbar';
 import './App.css';
+import Home from "./routes/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Code Nebula
-        </p>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ nebula/src/routes/Home.tsx
@@ -0,0 +1,14 @@
+import logo from '../logo.svg';
+
+const Home =()=>{
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Code Nebula
+      </p>
+    </header>
+  )
+}
+
+export default Home;
